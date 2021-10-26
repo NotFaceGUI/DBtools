8817cdbebb78cec8704a0cf03e32f034b7171077
NotFaceGUI <2509191792@qq.com>
2021-10-26 19:36:03 +0800

    图标类

---
+++ src/main/java/com/notface/util/icondata/IconData.java
@@ -0,0 +1,85 @@
+package com.notface.util.icondata;
+
+import javax.swing.*;
+import java.net.URL;
+
+public class IconData {
+    // 存放图片数据
+    private static URL copy = IconData.class.getResource("/image/copy.png");
+    public static ImageIcon copyIcon =  new ImageIcon(copy);
+
+    private static URL paste = IconData.class.getResource("/image/paste.png");
+    public static ImageIcon pasteIcon =  new ImageIcon(paste);
+
+    private static URL shear = IconData.class.getResource("/image/shear.png");
+    public static ImageIcon shearIcon =  new ImageIcon(shear);
+
+    private static URL refresh = IconData.class.getResource("/image/refresh.png");
+    public static ImageIcon refreshIcon =  new ImageIcon(refresh);
+
+    private static URL add = IconData.class.getResource("/image/add.png");
+    public static ImageIcon addIcon =  new ImageIcon(add);
+
+    private static URL reduce = IconData.class.getResource("/image/reduce.png");
+    public static ImageIcon reduceIcon =  new ImageIcon(reduce);
+
+    private static URL up = IconData.class.getResource("/image/up.png");
+    public static ImageIcon upIcon =  new ImageIcon(up);
+
+    private static URL down = IconData.class.getResource("/image/down.png");
+    public static ImageIcon downIcon =  new ImageIcon(down);
+
+    private static URL left = IconData.class.getResource("/image/Left.png");
+    public static ImageIcon leftIcon =  new ImageIcon(left);
+
+    private static URL right = IconData.class.getResource("/image/Right.png");
+    public static ImageIcon rightIcon =  new ImageIcon(right);
+
+    private static URL connect = IconData.class.getResource("/image/connect.png");
+    public static ImageIcon connectIcon =  new ImageIcon(connect);
+
+    private static URL library = IconData.class.getResource("/image/library.png");
+    public static ImageIcon libraryIcon =  new ImageIcon(library);
+
+    private static URL table = IconData.class.getResource("/image/table.png");
+    public static ImageIcon tableIcon =  new ImageIcon(table);
+
+    private static URL install = IconData.class.getResource("/image/install.png");
+    public static ImageIcon installIcon =  new ImageIcon(install);
+
+    private static URL host = IconData.class.getResource("/image/host.png");
+    public static ImageIcon hostIcon =  new ImageIcon(host);
+
+    private static URL port = IconData.class.getResource("/image/port.png");
+    public static ImageIcon portIcon =  new ImageIcon(port);
+
+    private static URL user = IconData.class.getResource("/image/user.png");
+    public static ImageIcon userIcon =  new ImageIcon(user);
+
+    private static URL ps = IconData.class.getResource("/image/password.png");
+    public static ImageIcon psIcon =  new ImageIcon(ps);
+
+    private static URL point = IconData.class.getResource("/image/point.png");
+    public static ImageIcon poIcon =  new ImageIcon(point);
+
+    private static URL save = IconData.class.getResource("/image/Save.png");
+    public static ImageIcon saveIcon =  new ImageIcon(save);
+
+    private static URL add_to = IconData.class.getResource("/image/add_to.png");
+    public static ImageIcon add_toIcon =  new ImageIcon(add_to);
+
+    private static URL insert = IconData.class.getResource("/image/insert.png");
+    public static ImageIcon insertIcon =  new ImageIcon(insert);
+
+    private static URL delete = IconData.class.getResource("/image/delete.png");
+    public static ImageIcon deleteIcon =  new ImageIcon(delete);
+
+    private static URL primary_key = IconData.class.getResource("/image/Primarykey.png");
+    public static ImageIcon primary_keyIcon =  new ImageIcon(primary_key);
+
+    private static URL excute = IconData.class.getResource("/image/excute.png");
+    public static ImageIcon excuteIcon =  new ImageIcon(excute);
+
+    private static URL word = IconData.class.getResource("/image/word.png");
+    public static ImageIcon wordIcon =  new ImageIcon(word);
+}
